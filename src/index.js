a11352f7d2c5809dc23f6192045d219492fa6b23
sergii <sergey.maksymenko@gmail.com>
2018-03-15 00:02:48 -0700

    magic notes initail implementation

--- src/index.js
+++ src/index.js
@@ -1,92 +1,40 @@
 'use strict';
 const Alexa = require('alexa-sdk');
 var request = require('request-promise');
-var sync_request = require('sync-request');
-var firebase = require('firebase');
-var admin = require('firebase-admin');
-const config = require('./firebase_config.json');
-const serviceAccount = require('./service_account.json');
-const async = require('async');
 
 exports.handler = (event, context, callback) => {
-  console.log('>>> event: ' + JSON.stringify(event));
-  context.callbackWaitsForEmptyEventLoop = true;
   var alexa = Alexa.handler(event, context);
-  alexa.appId = 'amzn1.ask.skill.5cfe4962-049f-4061-8176-3cb50878f279';
+  alexa.appId = 'amzn1.ask.skill.f40f64c9-b78b-4353-8a93-950427a958a6';
   alexa.registerHandlers(handlers);
   alexa.execute();
 };
 
-
 const handlers = {
   'LaunchRequest': function () {
-    this.emit('sensor');
+    this.emit('make');
   },
-  'sensor': function () {
+  'make': function () {
     var accessToken = this.event.session.user.accessToken;
     console.log('>>> token: ' + accessToken);
 
-    console.log('>>> this.event.session.user: ' + JSON.stringify(this.event.session.user));
-
-    var options = {
+    const options = {
       url: 'https://www.googleapis.com/userinfo/v2/me',
       headers: {
         'Authorization': 'Bearer ' + accessToken,
       }
     };
 
-    // request(options).then(resp => {
-    //   var userInfo = resp;
-    //   console.log('>>> userInfo: ' + JSON.stringify(userInfo));
+    request(options).then(resp => {
+      console.log('>>> resp: ' + resp);
+      const userInfo = JSON.parse(resp);
 
-    //   this.emit(':responseReady');
-    // }).catch(error => {
-    //   console.log('>>>1 error: ' + JSON.stringify(error));
-    //   this.emit(':responseReady');
-    // });
-
-
-    admin.initializeApp({
-      credential: admin.credential.cert(serviceAccount),
-      databaseURL: config.firebase.databaseURL
-    });
-
-    admin.auth().verifyIdToken(accessToken).then(resp => {
-      var user = resp;
-      console.log('>>> user: ' + JSON.stringify(user));
-
-      this.response.speak('Test message from Hocamee. new Account linked to ' + user.name);
+      this.response.speak('Test message from Smart Nodes. Account linked to ' + userInfo.name);
 
       this.emit(':responseReady');
     }).catch(error => {
       console.log('>>> error: ' + JSON.stringify(error));
       this.emit(':responseReady');
     });
-
-
-
-    var app = firebase.initializeApp({
-      credential: admin.credential.refreshToken(accessToken),
-      databaseURL: config.firebase.databaseURL
-    });
-    var firebaseDatabase = app.database();
-    console.log('>>> firebaseDatabase: ' + firebaseDatabase);
-
-    var res = sync_request('GET', 'https://www.googleapis.com/userinfo/v2/me', {
-      headers: {
-        'Authorization': 'Bearer ' + this.event.session.user.accessToken,
-      },
-    });
-    console.log('>>>> sync body:  ' + res.getBody())
-    if (res.getBody()) {
-      var profile = JSON.parse(res.getBody());
-
-      console.log('>>>> profile name:  ' + profile.name)
-
-      this.response.speak('Test message from Hocamee. Account linked to ' + profile.name);
-    } else {
-      this.response.speak('Test message from Hocamee. Link your Hocamee account to Alexa to access your sensors.');
-    }
   },
   'AMAZON.HelpIntent': function () {
     const speechOutput = 'This is the Hello World Sample Skill. ';
