b217cb1fa6d9280c2cb9ee6f6d286c173b9aa80c
sergii <sergey.maksymenko@gmail.com>
2018-03-14 23:19:12 -0700

    draft code sample

--- README.md
+++ README.md
@@ -1,30 +1,53 @@
 ## Starter guide to create Alexa skill
 
+### Create custom skill
 * Navigate `https://developer.amazon.com/home.html > ALEXA > Get Started > Create Skill`
+* Select `Custom` skill
 * Enter name: `Message Manager`
-* Clock: `Create skill`
+* Click: `Create skill`
 
 * Create `Invocation`  - phrase users say to begin interation with skill. For custom skill only.
   * `Intents` - represents action
   * `Slot` -  argument
 
-  * Create js app , zip ja file a long with dependencies and upload to lambda.
+### Add AWS lambda endpoint
+* create lambda `https://console.aws.amazon.com/lambda/home?region=us-east-1#/functions`
+  * create role for lambda from template `Simple Microservice permissions`
+  * click `Create`
+* Add trigger : `Alexa Skills Kit`
+
+## Link Alexa Skill and AWS lambda
+* Copy skill ID `amzn1.ask.skill.xxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxx` to labbda function trigger.
+* Copy ARN from AWS lambda and pasete to Alexa Skill `AWS Lambda ARN`
+
+### Account Linking
+* Enable account linking
+* Get `Authentication provider` details `https://console.developers.google.com` > `Credentials`
+  * `Create Credentians` > `OAuth Client ID` > `Web application`
+  * Add authorised redirect urls from Alexa skill `Account linking` page.
+  * `Create`  and copy client ID and client secret to Alexa skill 'Account linking` page.
+  * `Download JSON` with connection details and put parameters on Alexa skill page.
+* Configure `Alexa skill`
+  * Choose: `Auth Code Grant`
+  * Scope: `openid`
+  * past: token URI  and Auth URI from downloaded configuration file.
+
+### Launch Skill in Beta testing mode
+* To test if Account linking works 
+  * click `Launch` > `Save and Continue` > `Save and Continue`
+  * copy invitation link or invite by mail.
+
+### Add Implementation
+* Create NodeJS project 
+* Implement required functionality see `index.js` as sample.
+* Package all sources and dependencies (node_modules) into single zip file and deploy application to AWS lambda.
 
 
 
-* Use `Account Linking` to connect end-user to use account in other system.
-  * https://developer.amazon.com/docs/custom-skills/link-an-alexa-user-with-a-user-in-your-system.html 
-  * https://developer.amazon.com/post/Tx3CX1ETRZZ2NPC/Alexa-Account-Linking-5-Steps-to-Seamlessly-Link-Your-Alexa-Skill-with-Login-wit
-  * sample https://github.com/patrick-michelberger/hitch/blob/master/src/skill.js
-
-
-
-... Custom Skills `Steps to Build a Custom Skill`
-
-... https://developer.amazon.com/docs/custom-skills/steps-to-build-a-custom-skill.html#step-1-design-a-voice-user-interface
-
-
+----
 ### Resources
+* https://developer.amazon.com/docs/custom-skills/link-an-alexa-user-with-a-user-in-your-system.html 
+  * sample * sample https://github.com/patrick-michelberger/hitch/blob/master/src/skill.js
 * events: https://developer.amazon.com/docs/smarthome/send-events-to-the-alexa-event-gateway.html
 * custom skill: https://developer.amazon.com/docs/custom-skills/understanding-custom-skills.html
   * https://developer.amazon.com/docs/custom-skills/steps-to-build-a-custom-skill.html
--- src/index.js
+++ src/index.js
@@ -1,43 +1,76 @@
 'use strict';
 const Alexa = require('alexa-sdk');
-var request = require('request');
+var request = require('request-promise');
 var sync_request = require('sync-request');
+var firebase = require('firebase');
+var admin = require('firebase-admin');
+const config = require('./firebase_config.json');
+const serviceAccount = require('./service_account.json');
+const async = require('async');
 
 exports.handler = (event, context, callback) => {
+  console.log('>>> event: ' + JSON.stringify(event));
+  context.callbackWaitsForEmptyEventLoop = true;
   var alexa = Alexa.handler(event, context);
   alexa.appId = 'amzn1.ask.skill.5cfe4962-049f-4061-8176-3cb50878f279';
   alexa.registerHandlers(handlers);
   alexa.execute();
 };
 
+
 const handlers = {
   'LaunchRequest': function () {
     this.emit('sensor');
   },
   'sensor': function () {
-    console.log('>>> token: ' + this.event.session.user.accessToken);
+    var accessToken = this.event.session.user.accessToken;
+    console.log('>>> token: ' + accessToken);
+
+    console.log('>>> this.event.session.user: ' + JSON.stringify(this.event.session.user));
 
     var options = {
       url: 'https://www.googleapis.com/userinfo/v2/me',
       headers: {
-        'Authorization': 'Bearer ' + this.event.session.user.accessToken,
+        'Authorization': 'Bearer ' + accessToken,
       }
     };
 
-    function callback(error, response, body) {
-      console.log('>> resp code: ' + response.statusCode);
-      console.log('>> error: ' + error);
-      if (!error && response.statusCode == 200) {
-        console.log('>>> body:  ' + body);
-        console.log('>>> JSON body:  ' + JSON.stringify(body));
-        var info = JSON.parse(body);
-        console.log('>>> JSON info:  ' + JSON.stringify(info));
-      }
-    }
-    console.log('>>> ask profile')
-    request(options, callback).on('error', function (err) {
-      console.log(err)
+    // request(options).then(resp => {
+    //   var userInfo = resp;
+    //   console.log('>>> userInfo: ' + JSON.stringify(userInfo));
+
+    //   this.emit(':responseReady');
+    // }).catch(error => {
+    //   console.log('>>>1 error: ' + JSON.stringify(error));
+    //   this.emit(':responseReady');
+    // });
+
+
+    admin.initializeApp({
+      credential: admin.credential.cert(serviceAccount),
+      databaseURL: config.firebase.databaseURL
     });
+
+    admin.auth().verifyIdToken(accessToken).then(resp => {
+      var user = resp;
+      console.log('>>> user: ' + JSON.stringify(user));
+
+      this.response.speak('Test message from Hocamee. new Account linked to ' + user.name);
+
+      this.emit(':responseReady');
+    }).catch(error => {
+      console.log('>>> error: ' + JSON.stringify(error));
+      this.emit(':responseReady');
+    });
+
+
+
+    var app = firebase.initializeApp({
+      credential: admin.credential.refreshToken(accessToken),
+      databaseURL: config.firebase.databaseURL
+    });
+    var firebaseDatabase = app.database();
+    console.log('>>> firebaseDatabase: ' + firebaseDatabase);
 
     var res = sync_request('GET', 'https://www.googleapis.com/userinfo/v2/me', {
       headers: {
@@ -54,10 +87,6 @@ const handlers = {
     } else {
       this.response.speak('Test message from Hocamee. Link your Hocamee account to Alexa to access your sensors.');
     }
-
-
-
-    this.emit(':responseReady');
   },
   'AMAZON.HelpIntent': function () {
     const speechOutput = 'This is the Hello World Sample Skill. ';
